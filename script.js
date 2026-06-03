9c6506b6ec09d4295e3f8e9f8c63befb65bf1fb3
reyzm7 <maelfrx59@gmail.com>
2026-06-03 11:14:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
-console.log("ModBot chargé");
+window.addEventListener("load", () => {
+    console.log("ModBot prêt !");
+});
 
 const cards = document.querySelectorAll(".card");
 
-const observer = new IntersectionObserver(entries => {
-
-entries.forEach(entry => {
-
-if(entry.isIntersecting){
-
-entry.target.style.opacity = "1";
-entry.target.style.transform = "translateY(0)";
-
-}
-
-});
-
-});
-
 cards.forEach(card => {
+    card.addEventListener("mouseenter", () => {
+        card.style.transform = "translateY(-5px)";
+    });
 
-card.style.opacity = "0";
-card.style.transform = "translateY(40px)";
-card.style.transition = "0.6s";
-
-observer.observe(card);
-
+    card.addEventListener("mouseleave", () => {
+        card.style.transform = "translateY(0)";
+    });
 });
